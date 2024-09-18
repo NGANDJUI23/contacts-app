eb3d4a38cafea088d1bead4b59291953edb29c0e
NGANDJUI NANGA Ivan Delors <ngandjui.ivan@institutsaintjean.org>
2024-09-18 20:50:25 +0200

    Implementation de la fontionnalite de filtre

--- src/app/components/cool-contacts/cool-contacts.component.ts
+++ src/app/components/cool-contacts/cool-contacts.component.ts
@@ -1,14 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { ContactComponent } from '../contact/contact.component';
 import { ContactService } from '../../../services_contact/contact.service';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-cool-contacts',
   standalone: true,
-  imports: [ContactComponent],
+  imports: [ContactComponent, FormsModule],
   templateUrl: './cool-contacts.component.html',
   styleUrl: './cool-contacts.component.scss'
 })
 export class CoolContactsComponent {
-  service_contact = inject(ContactService)
+  service_contact = inject(ContactService);
+  list = this.service_contact.contact_pomme_fruit_favoris();
   constructor() { }
+  filteredContact = [...this.list];
+  filterText: string = '';
+
+  async filter_1() {
+    this.filteredContact = this.list.filter(contact =>
+      contact["eyeColor"].toLowerCase().includes(this.filterText.toLowerCase())
+    );
+  }
+  async filter_2() {
+    this.filteredContact = this.list.filter(contact =>
+      contact["favoriteFruit"].toLowerCase().includes(this.filterText.toLowerCase())
+    );
+  }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -8,4 +9,8 @@ import { Component, Input } from '@angular/core';
 })
 export class NavbarComponent {
   @Input() color!: string;
+  async presentAlert() {
+    await alert("bonjpour");
+
+  }
 }
